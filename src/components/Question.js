c25ca43e5cc50a95ecd325afc4c2e3b6c5a2f883
kelly_maass <kmaass77627@gmail.com>
2023-11-28 21:28:36 -0600

    done

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() => {
+   const timer = setTimeout(() => {
+      setTimeRemaining(timeRemaining - 1)
+      if (timeRemaining === 0) {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+    return function cleanUp() {
+      clearInterval(timer)
+    }
+  }, [timeRemaining, onAnswered])
+
+  
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
